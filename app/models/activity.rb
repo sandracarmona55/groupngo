50c120ee77ff048aa46ca0a0aafb2c0c77592351
Sandra Carmona <sandracarmona55@gmail.com>
2019-12-10 12:35:12 +0100

    Added fields to user and validations to review

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,3 +1,5 @@
 class Activity < ApplicationRecord
   has_many :groups
+  CATEGORY = ["culture", "sports", "gastronomy", "leisure", "relaxation", "concerts"]
+  validates :category, inclusion: { in: CATEGORY}
 end
