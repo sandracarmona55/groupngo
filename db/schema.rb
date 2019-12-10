de7976b81564a7d77b31f105b67b9b6806a33b92
Cassandre Houze <cassandredelaulnoit@gmail.com>
2019-12-10 11:51:32 +0100

    add photo url to activity

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_10_101900) do
+ActiveRecord::Schema.define(version: 2019_12_10_104822) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2019_12_10_101900) do
     t.integer "deadline"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "photo_url"
   end
 
   create_table "users", force: :cascade do |t|
