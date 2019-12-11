079cbcfe81d2c28ea8d511a58fc7c1c68405c781
Sandra Carmona <sandracarmona55@gmail.com>
2019-12-11 11:29:49 +0100

    Route groups done

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  get 'activities/index'
-  get 'activities/show'
+
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :activities, only: [:show, :index]
+  resources :activities, only: [:index, :show] do
+    resources :groups, only: [:index]
+  end
 end
