b0e0d8d589cf81129e02488c28d5284b46872aee
Alexander Hollmann <alexander.hollmann@whu.edu>
2019-12-12 15:31:23 +0100

    added border to the buttons

---
+++ app/javascript/packs/show.js
@@ -0,0 +1,30 @@
+const openTab = () => {
+
+const buttonjoin = document.getElementById("btn-join");
+
+const buttondescription = document.getElementById("btn-description");
+
+const join = document.getElementById("join");
+console.log(join);
+
+const description = document.getElementById("description");
+console.log(description)
+
+buttonjoin.addEventListener("click", (event) => {
+  description.style.display = "none";
+  join.style.display = "block";
+  buttonjoin.className += " active";
+  buttondescription.classList.remove("active");
+
+});
+
+buttondescription.addEventListener("click", (event) => {
+  description.style.display = "block";
+  join.style.display = "none";
+  buttondescription.className += " active";
+  buttonjoin.classList.remove("active");
+});
+};
+
+export { openTab };
+
