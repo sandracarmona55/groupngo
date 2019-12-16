a60d326b68201a3bde2f3607fb0552cc5a07f9f9
Alexander Hollmann <alexander.hollmann@whu.edu>
2019-12-16 15:39:00 +0100

    added stars2

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :booking
   validates :title, presence: true
-  validates :content, presence: true
+  validates :content, length: { minimum: 130 }, presence: true
   validates :rating, presence: true
 end
