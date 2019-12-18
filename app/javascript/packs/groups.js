f520ee0509a2ada7c4778c9c0200bf6406e94901
Sandra Carmona <sandracarmona55@gmail.com>
2019-12-18 16:20:50 +0100

    Slots available added but there is a bug

--- app/javascript/packs/groups.js
+++ app/javascript/packs/groups.js
@@ -1,21 +1,44 @@
 // const time = document.querySelectorAll(".time-each");
 // console.log(time);
+const insertOptions = (spots) => {  // spots is an integer!
+  document.getElementById('booking_quantity').insertHTML = '' // clear the content
+  spots = Number(spots)
+
+  for (let s = 1; s < (spots + 1); s++) {
+    console.log(s, spots);
+    const choose_spot = document.getElementById('booking_quantity')
+    choose_spot.insertAdjacentHTML("beforeend", `<option value="${s}">${s}</option>`);
+  }
+
+  // spots times do
+  // interpolationnn
+  // end
+};
+
 const groups = () => {
 
 const join = document.getElementById('join-btn');
   const buttons = document.querySelectorAll('.time-each');
   buttons.forEach(btn => {
-    btn.addEventListener('click', (event) => {
+    btn.addEventListener('click', (evenIdt) => {
       buttons.forEach(btn => {
         btn.classList.remove('time-selected')
       })
       event.currentTarget.classList.add('time-selected')
-      const target = event.currentTarget.dataset.checkId
-      document.getElementById(`check-g-${target}`).click()
+      const targetId = event.currentTarget.dataset.checkId
+      const checkButton = document.getElementById(`check-g-${targetId}`)
+      checkButton.click()
       join.classList.add('join-group-selected')
       join.disabled = false
+      const spots = checkButton.dataset.spots
+      console.log(event.target, "test")
+      if (event.target == event.currentTarget) {
+        insertOptions(spots);
+      }
     })
   })
 };
 
+
 export { groups };
+
